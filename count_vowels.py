663d6730387c271f8768cb9a78a2d00c4fc0253e
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:38:41 -0600

    gitautosync update 39

---
+++ count_vowels.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# this program will take a filename as an argument,
+# and will print the number of vowels (a, e, i, o, u)
+# it finds in that file.  (We assume it's a text file.)
+
