2656c827020a113be13ea16a909d321a231b94e4
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:33:18 -0600

    gitautosync update 31

---
+++ calc2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# I want y to get a default value of 10.
+# If I just pass x, it'll add that value to 10.
+
+import argparse
+
+# create a parser
+parser = argparse.ArgumentParser()
+
+# declare arguments
+parser.add_argument('x', help='first numeric argument', type=int)  
+parser.add_argument('y', help='second numeric argument', type=int, default=10)
+
+# gather arguments
+args = parser.parse_args()
+
+# retrieve values
+print(f'{args.x} + {args.y} = {args.x + args.y}')
