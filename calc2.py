60e4295dc88e707e3a52f7416f80769058a76982
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:35:13 -0600

    gitautosync update 34

--- calc2.py
+++ calc2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
-# I want y to get a default value of 10.
-# If I just pass x, it'll add that value to 10.
+# what if I want to pass the arguments in any order?
+# To do that, I'll need to change them to be *keyword* arguments
+# In other words, I'll have to say -x 5 and -y 3
+# but I'll also be able to say -y 3 and -x 5, in that order
 
 import argparse
 
@@ -9,8 +11,9 @@ import argparse
 parser = argparse.ArgumentParser()
 
 # declare arguments
-parser.add_argument('x', help='first numeric argument', type=int)  
-parser.add_argument('y', help='second numeric argument', type=int)
+# having a - before the name of the argument makes it a keyword argument
+parser.add_argument('-x', help='first numeric argument', type=int)  
+parser.add_argument('-y', help='second numeric argument', type=int)
 
 # gather arguments
 args = parser.parse_args()
