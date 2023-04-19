cf07bd0325cf51def78a1e946967e4e8fc883b13
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:57:53 -0600

    gitautosync update 58

--- tupleargs.py
+++ tupleargs.py
@@ -4,6 +4,11 @@ import argparse
 
 parser = argparse.ArgumentParser()
 
+class LoudTuple:
+    def __init__(self, x):
+        print(f'I got {x}!')
+        self.x = tuple(x)
+
 parser.add_argument('t', help='tuple of stuff', type=tuple)
 parser.add_argument('n', help='more than one!', type=int, nargs='+')
 
