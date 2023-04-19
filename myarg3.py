934cdb406b7b71b7114c3ef024fad2411750bc4d
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:25:30 -0600

    gitautosync update 19

---
+++ myarg3.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import argparse
+
+# create a parser
+parser = argparse.ArgumentParser()
+
+# declare arguments
+parser.add_argument('x')   # any argument I define with add_argument...
+parser.add_argument('y')
+
+# gather arguments
+args = parser.parse_args()
+
+# retrieve values
+print(args.x)              # ... can be retrieved as an attribute from our parsed args
+print(args.y)
+
