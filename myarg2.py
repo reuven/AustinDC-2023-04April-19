a46f56b011490994bfe5086777f67990c525be1f
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:22:53 -0600

    gitautosync update 15

---
+++ myarg2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import argparse
+
+# create a parser
+parser = argparse.ArgumentParser()
+
+# declare arguments
+# (none for now)
+
+# gather arguments
+args = parser.parse_args()
+
+# retrieve values
+print(args)
