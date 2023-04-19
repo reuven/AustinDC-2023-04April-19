3fbd5425e4231de6651c63f6ee913950c5fdf80a
Reuven Lerner <reuven@lerner.co.il>
2023-04-18 18:39:32 -0600

    gitautosync update 40

--- count_vowels.py
+++ count_vowels.py
@@ -4,3 +4,21 @@
 # and will print the number of vowels (a, e, i, o, u)
 # it finds in that file.  (We assume it's a text file.)
 
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-f', help='name of the file', required=True)  
+
+# gather arguments
+args = parser.parse_args()
+
+# retrieve values
+total = 0
+
+for one_line in open(args.f):
+    for one_character in one_line:
+        if one_character in 'aeiou':
+            total += 1
+            
+print(f'Vowel count: {total}')            
